f4bd6acd28c981fbec7079fcf4e2e07d271a270a
sjnasr <sjnasr18@yahoo.com>
2019-02-25 16:08:47 -0800

    Hopefully the database file will be added

--- app/src/main/java/com/saranasr/comicbooksearch/MainActivity.java
+++ app/src/main/java/com/saranasr/comicbooksearch/MainActivity.java
@@ -6,12 +6,32 @@ import android.widget.ListView;
 import android.widget.SearchView;
 
 import java.util.ArrayList;
+import java.io.IOException;
+
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseHelper mDBHelper;
+    private SQLiteDatabase mDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDBHelper = new DatabaseHelper(this);
+
+        try {
+            mDBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
+
+        try {
+            mDb = mDBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
     }
 }
